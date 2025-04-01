1256d08e52b8c995563a35efe097416f0cbc9c0a
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2025-04-01 09:51:05 -0400

    fix: fix typos in config files

--- terramedic/tailwind.config.js
+++ terramedic/tailwind.config.js
@@ -2,8 +2,8 @@
 export default {
   content: [
     './src/**/*.{html,js,svelte,ts}',
-    './node_modules/flowbite/**/*.js'
-      './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite/**/*.js',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
   ],
   theme: {
     extend: {

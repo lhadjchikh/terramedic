bbd1c7781a6d6807489e75a7672b739239be8f90
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2025-04-01 18:34:01 -0400

    feat: clean up navbar [WIP]

--- terramedic/src/lib/components/NavBar.svelte
+++ terramedic/src/lib/components/NavBar.svelte
@@ -9,37 +9,27 @@
   if (typeof window !== 'undefined') {
     activeUrl = window.location.pathname;
   }
-
-  // Toggle the hidden state
-  function toggle() {
-    hidden = !hidden;
-    console.log('Hidden state:', hidden); // Debugging
-  }
 </script>
 
 <div class="navbar-background relative overflow-hidden">
   <!-- Navbar Content -->
   <Navbar
     color="white"
-    class="bg-opacity-75 relative z-10 border-b border-gray-200 bg-white px-4 py-2.5 lg:px-6"
+    class="relative z-10 border-b border-gray-200 bg-white bg-opacity-75 px-4 py-2.5 lg:px-6"
   >
     <NavBrand href="/" class="relative flex items-center justify-center rounded-md bg-white p-2">
       <div class="logo-container">
         <Logo size="small" class="logo" />
       </div>
     </NavBrand>
-    <NavHamburger on:click={toggle} class="relative block rounded-md bg-white p-2 shadow-md" />
-    <NavUl
-      class={`order-1 w-full transition-all duration-300 md:order-none md:flex md:w-auto ${
-        hidden ? 'max-h-0 overflow-hidden' : 'max-h-screen'
-      }`}
-    >
-      <NavLi href="/" active={activeUrl === '/'}>Home</NavLi>
-      <NavLi href="/about" active={activeUrl === '/about'}>About</NavLi>
-      <NavLi href="/volunteer" active={activeUrl === '/volunteer'}>Volunteer</NavLi>
-      <NavLi href="/donate" active={activeUrl === '/donate'}>Donate</NavLi>
-      <NavLi href="/other-ways" active={activeUrl === '/other-ways'}>Other Ways</NavLi>
-      <NavLi href="/resources" active={activeUrl === '/resources'}>Resources</NavLi>
+    <NavHamburger class="block rounded-md bg-white p-2 shadow-md" />
+    <NavUl class="duration-30 w-fulltransition-all order-1 flex flex-col items-end">
+      <NavLi class="my-2" href="/" active={activeUrl === '/'}>Home</NavLi>
+      <NavLi class="my-2" href="/about" active={activeUrl === '/about'}>About</NavLi>
+      <NavLi class="my-2" href="/volunteer" active={activeUrl === '/volunteer'}>Volunteer</NavLi>
+      <NavLi class="my-2" href="/donate" active={activeUrl === '/donate'}>Donate</NavLi>
+      <NavLi class="my-2" href="/other-ways" active={activeUrl === '/other-ways'}>Other Ways</NavLi>
+      <NavLi class="my-2" href="/resources" active={activeUrl === '/resources'}>Resources</NavLi>
     </NavUl>
   </Navbar>
 </div>
